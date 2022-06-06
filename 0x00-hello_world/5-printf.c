154b3bf938ab595812146adabe20598692ddac84
ErmiasYehuala <ermiaszewdu18@gmail.com>
2022-06-06 14:38:06 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  *  * main - Entry point
  *   * display message onscreen
  *    * Return: Always 0 (Success)
- *     */
+ *     **/
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
